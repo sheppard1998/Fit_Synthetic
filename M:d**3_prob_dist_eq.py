01dd92238f74062962381df8f2e02694ade1f191
Sam Sheppard <ssheppa1@dhcp-130-58-92-238.swarthmore.edu>
2019-07-09 15:21:31 -0400

    Coded the current probability ration distribution equation in a separate python file

--- M:d**3_prob_dist_eq.py
+++ M:d**3_prob_dist_eq.py
@@ -10,14 +10,13 @@ import numpy as np
 
 from scipy import special
 
-def mass_distance_prior(m, m_mea*--------------
-                        n, m_dev, d, d_mean, d_dev):
+def mass_distance_prior(m, m_mean, m_dev, d, d_mean, d_dev):
     
     r = (m - m_mean)/((d - d_mean)**3)
     
     fresnelS, fresnelC = special.fresnel(m_dev/np.sqrt(np.pi*r*m_dev*d_dev**3))
     
-    prob = (np.sqrt(m_dev)/((4*d_dev*r**2)**(3/2)))*(np.pi*np.cos(m_dev/(2*r*d_dev**3))*(1-2*fresnelC) \
-                   + np.pi*np.sin(m_dev/(2*r*d_dev**3))*(1-2*fresnelS))
+    prob = (np.sqrt(m_dev)/((4*d_dev*r**2)**(3/2)))*(np.pi*np.cos(m_dev/ \
+           (2*r*d_dev**3))*(1-2*fresnelC) + np.pi*np.sin(m_dev/(2*r*d_dev**3))*(1-2*fresnelS))
     
     return prob
